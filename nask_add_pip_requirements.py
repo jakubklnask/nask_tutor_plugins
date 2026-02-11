a667a0c3d45e901239395b62da17ea94267ffa2d
jakubkl <jakubkl@nask.pl>
2026-02-11 14:31:51 +0100

    fixed - posted plugin was a test one - now correct one

--- nask_add_pip_requirements.py
+++ nask_add_pip_requirements.py
@@ -3,7 +3,7 @@ from tutor import hooks
 
 # override the config value
 hooks.Filters.CONFIG_OVERRIDES.add_item(
-    ("OPENEDX_EXTRA_PIP_REQUIREMENTS", "TESTgit+https://github.com/HAAAAA/nask_filters@main")
+    ("OPENEDX_EXTRA_PIP_REQUIREMENTS", "git+https://github.com/jakubklnask/nask_filters@main")
 )
     
 
