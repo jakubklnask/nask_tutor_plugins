20e730af85d6c256527684fa7253e17ecba5de2b
jakubkl <jakubkl@nask.pl>
2026-02-11 14:30:42 +0100

    added plugin that links the required filter via a OPENEDX_EXTRA_PIP_REQUIREMENTS config value for proper image building

---
+++ nask_add_pip_requirements.py
@@ -0,0 +1,9 @@
+
+from tutor import hooks
+
+# override the config value
+hooks.Filters.CONFIG_OVERRIDES.add_item(
+    ("OPENEDX_EXTRA_PIP_REQUIREMENTS", "TESTgit+https://github.com/HAAAAA/nask_filters@main")
+)
+    
+
