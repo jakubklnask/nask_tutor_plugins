2f5e0a8b9edf08bf399a757d2f7c69d3c80c857e
jakubkl <jakubkl@nask.pl>
2026-02-17 11:18:47 +0100

    added new pip package to requirements

--- nask_add_pip_requirements.py
+++ nask_add_pip_requirements.py
@@ -3,7 +3,10 @@ from tutor import hooks
 
 # override the config value
 hooks.Filters.CONFIG_OVERRIDES.add_item(
-    ("OPENEDX_EXTRA_PIP_REQUIREMENTS", "git+https://github.com/jakubklnask/nask_filters@main")
+    ("OPENEDX_EXTRA_PIP_REQUIREMENTS", [
+        "git+https://github.com/jakubklnask/nask_filters@main",
+        "git+https://github.com/jakubklnask/nask_azure_auth@main"
+    ])
 )
     
 
