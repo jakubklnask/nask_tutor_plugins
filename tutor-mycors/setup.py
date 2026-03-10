fa70b7f21b7a89fbfcd42848eb2963a323b35f34
jakubkl <jakubkl@nask.pl>
2026-03-10 12:31:55 +0100

    fixed tutor-mycors plugin - now has all 5 domains and correct https settings

--- tutor-mycors/setup.py
+++ tutor-mycors/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name="tutor-mycors",
-    version="0.1.0",
-    description="Tutor plugin to add multiple CORS/CSRF origins for edutechnologie.sp.nask.pl, apps.edutechnologie.sp.nask.pl and studio.edutechnologie.sp.nask.pl",
+    version="0.2.0",
+    description="Tutor plugin to add multiple CORS/CSRF origins for edu.technologie.sp.nask.pl, apps.edu.technologie.sp.nask.pl and studio.edu.technologie.sp.nask.pl",
     packages=["mycors"],
     entry_points={
         "tutor.plugin.v1": [
