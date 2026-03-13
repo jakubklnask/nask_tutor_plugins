b2508ae30781c58377149e3ee85acb6ccd4011a6
jakubkl <jakubkl@nask.pl>
2026-03-13 16:15:39 +0100

    disabling EDX_ACE for prod as well

--- nask_mail_settings.py
+++ nask_mail_settings.py
@@ -11,6 +11,12 @@ hooks.Filters.ENV_PATCHES.add_items([
 
     ("openedx-lms-development-settings", 'BULK_EMAIL_SEND_USING_EDX_ACE = False'),
     ("openedx-cms-development-settings", 'BULK_EMAIL_SEND_USING_EDX_ACE = False'),
+
+    ("openedx-lms-production-settings", 'EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend" ; EMAIL_USE_COURSE_ID_FROM_FOR_BULK = False ; BULK_EMAIL_DEFAULT_FROM_EMAIL = "edu.technologie@nask.pl" ; DEFAULT_FROM_EMAIL = "edu.technologie@nask.pl"'),
+    ("openedx-cms-production-settings", 'EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend" ; DEFAULT_FROM_EMAIL = "edu.technologie@nask.pl"'),
+
+    ("openedx-lms-production-settings", 'BULK_EMAIL_SEND_USING_EDX_ACE = False'),
+    ("openedx-cms-production-settings", 'BULK_EMAIL_SEND_USING_EDX_ACE = False'),
     
     
     # Ustawienia YAML (rozbite na osobne wpisy, żeby uniknąć przecinków i \n)
