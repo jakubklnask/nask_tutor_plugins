fb30c73c795993ed3aef46cb5a7c30065aa85cd3
jakubkl <jakubkl@nask.pl>
2026-03-13 13:26:36 +0100

    specified subdir of moved plugin (pip package)

--- nask_add_pip_requirements.py
+++ nask_add_pip_requirements.py
@@ -4,6 +4,7 @@ from tutor import hooks
 hooks.Filters.CONFIG_OVERRIDES.add_item(
     ("OPENEDX_EXTRA_PIP_REQUIREMENTS", [
         "git+https://github.com/jakubklnask/openedx-pip-packages@main#subdirectory=nask_azure_auth",
-        "git+https://github.com/jakubklnask/openedx-pip-packages@main#subdirectory=nask_filters"
+        "git+https://github.com/jakubklnask/openedx-pip-packages@main#subdirectory=nask_filters",
+        "git+https://github.com/jakubklnask/openedx-pip-packages@main#subdirectory=tutor-mycors"
     ])
 )
